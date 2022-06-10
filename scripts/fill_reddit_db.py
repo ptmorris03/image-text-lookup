6c18cfeb27e7e35441e5ea57cefeeb9adb2e7bca
Paul Morris <pmorris2012@fau.edu>
2022-06-10 11:37:30 -0400

    test reading from comment dump

---
+++ scripts/fill_reddit_db.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+from tqdm import tqdm
+import typer
+import zreader
+import ujson as json
+
+from pathlib import Path
+
+
+def parse_comment(comment):
+    return {
+        "_id": 0
+    }
+
+def insert_comments(db, comments):
+    comments = map(parse_comment, comments)
+    return db.comments.insert_many(comments, ordered=False)
+
+def main(
+    file: Path,
+    host: str = "localhost", 
+    port: int = 27017):
+    #client = MongoClient(F"{host}:{port}")
+    #db = client.reddit
+
+    #result = insert_comments(db, comments)
+
+    zreader = zreader.Zreader(file)
+    for line in zreader.read():
+        obj = json.loads(line)
+        print(obj)
+        break
+
+if __name__ == "__main__":
+    typer.run(main)
