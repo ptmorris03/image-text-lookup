cfb080ba299c4a845eda8c1059deb222b6c903ee
Paul Morris <pmorris2012@fau.edu>
2022-06-14 19:20:50 -0400

    count_records

--- scripts/count_db.py
+++ scripts/count_db.py
@@ -11,10 +11,9 @@ def main(
     ):
     client = MongoClient(F"{host}:{port}")
     db = getattr(client, db_name)
-    result = db.runCommand({
-        "count": collection_name
-    })
-    print(F"{result['n']} documents in {db_name}.{collection_name}")
+    col = getattr(db, collection_name)
+    count = col.countDocuments(query="")
+    print(F"{count} documents in {db_name}.{collection_name}")
 
 if __name__ == "__main__":
     typer.run(main)
